33f8f78203f61bd00b2a5391e4d5ba5fcdbf5630
timothyteguh1 <c14230133@john.petra.ac.id>
2026-03-26 11:01:34 +0700

    s

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display:inline-block; text-decoration:none;">
+    <img src="{{ asset('logo-blokpedia.png') }}"
+         alt="Blockped"
+         style="width:55px; height:55px; border-radius:50%; display:block; margin:0 auto;">
+    <span style="display:block; text-align:center; font-weight:900; 
+                 font-size:18px; color:#1a202c; margin-top:8px; 
+                 letter-spacing:2px;">BLOCKPED</span>
+</a>
+</td>
+</tr>
